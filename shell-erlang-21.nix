260cd0bad0e1c898df9d5017dd8083d82820c12f
philipstears <philip@philipstears.com>
2019-07-09 16:34:21 +0200

    Erlang 21 shell.

---
+++ shell-erlang-21.nix
@@ -0,0 +1,14 @@
+with import <nixpkgs> {
+  overlays = [
+    (import ./.)
+  ];
+};
+
+stdenv.mkDerivation {
+  name = "example-build-shell";
+  buildInputs = [
+    pkgs.devPackages.erlang-21-2-4.erlang
+    pkgs.devPackages.erlang-21-2-4.rebar3-9
+  ];
+}
+
