71cb7f80484e443bc870b647e775d70f3b00cf93
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2020-02-10 16:00:12 +0000

    purerl 0.0.2

--- purerl-overlay.nix
+++ purerl-overlay.nix
@@ -2,6 +2,7 @@ self: super:
 {
   purerl = (super.purerl or {}) // {
     purerl-0-0-1 = super.callPackage ./purerl/0.0.1.nix {};
+    purerl-0-0-2 = super.callPackage ./purerl/0.0.2.nix {};
   };
 }
 
