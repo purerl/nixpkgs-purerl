08e0fecd8e12727d2808367f64731436db0894b0
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2020-07-07 08:54:50 +0000

    0.0.6

--- purerl-overlay.nix
+++ purerl-overlay.nix
@@ -6,6 +6,7 @@ self: super:
     # 0.0.3 reported the wrong version and should not be used, 0.0.4 is functionally the same
     purerl-0-0-4 = super.callPackage ./purerl/0.0.4.nix {};
     purerl-0-0-5 = super.callPackage ./purerl/0.0.5.nix {};
+    purerl-0-0-6 = super.callPackage ./purerl/0.0.6.nix {};
   };
 }
 
