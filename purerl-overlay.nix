26f97f138f58ab3b81574696b63f52493d014742
Steve Strong <steve@srstrong.com>
2023-07-03 10:15:25 +0100

    Added 0.0.20

--- purerl-overlay.nix
+++ purerl-overlay.nix
@@ -20,5 +20,6 @@ self: super:
     purerl-0-0-17 = super.callPackage ./purerl/0.0.17.nix {};
     purerl-0-0-18 = super.callPackage ./purerl/0.0.18.nix {};
     purerl-0-0-19 = super.callPackage ./purerl/0.0.19.nix {};
+    purerl-0-0-20 = super.callPackage ./purerl/0.0.20.nix {};
   };
 }
